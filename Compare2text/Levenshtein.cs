6a15b8627651cb7ccb5196f01cea570c8e21ea7c
Azizjon Muminov <Azizjon.Muminov@finca.tj>
2020-05-19 16:57:47 +0500

    Add project files.

---
+++ Compare2text/Levenshtein.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Compare2text
+{
+    public class Levenshtein
+    {
+        public static int DamerauLevenshteinDistance(string source, string target, int threshold)
+        {
+
+            int length1 = source.Length;
+            int length2 = target.Length;
+
+            if (Math.Abs(length1 - length2) > threshold) { return int.MaxValue; }
+
+            if (length1 > length2)
+            {
+                Swap(ref target, ref source);
+                Swap(ref length1, ref length2);
+            }
+
+            int maxi = length1;
+            int maxj = length2;
+
+            int[] dCurrent = new int[maxi + 1];
+            int[] dMinus1 = new int[maxi + 1];
+            int[] dMinus2 = new int[maxi + 1];
+            int[] dSwap;
+
+            for (int i = 0; i <= maxi; i++) { dCurrent[i] = i; }
+
+            int jm1 = 0, im1 = 0, im2 = -1;
+
+            for (int j = 1; j <= maxj; j++)
+            {
+
+                dSwap = dMinus2;
+                dMinus2 = dMinus1;
+                dMinus1 = dCurrent;
+                dCurrent = dSwap;
+
+                int minDistance = int.MaxValue;
+                dCurrent[0] = j;
+                im1 = 0;
+                im2 = -1;
+
+                for (int i = 1; i <= maxi; i++)
+                {
+
+                    int cost = source[im1] == target[jm1] ? 0 : 1;
+
+                    int del = dCurrent[im1] + 1;
+                    int ins = dMinus1[i] + 1;
+                    int sub = dMinus1[im1] + cost;
+
+                    int min = (del > ins) ? (ins > sub ? sub : ins) : (del > sub ? sub : del);
+
+                    if (i > 1 && j > 1 && source[im2] == target[jm1] && source[im1] == target[j - 2])
+                    {
+                        min = Math.Min(min, dMinus2[im2] + cost);
+                    }
+
+                    dCurrent[i] = min;
+                    if (min < minDistance) { minDistance = min; }
+                    im1++;
+                    im2++;
+                }
+                jm1++;
+                if (minDistance > threshold) { return int.MaxValue; }
+            }
+
+            int result = dCurrent[maxi];
+            return (result > threshold) ? int.MaxValue : result;
+        }
+        static void Swap<T>(ref T arg1, ref T arg2)
+        {
+            T temp = arg1;
+            arg1 = arg2;
+            arg2 = temp;
+        }
+        public static double LevenshteinMatch(string source, string target)
+        {
+            double maxLength = Math.Max(source.Length, target.Length);
+            return 1 - (DamerauLevenshteinDistance(source, target, (int)maxLength) / maxLength);
+        }
+    }
+}
---
+++ Compare2text/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compare2text
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CompareBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var textR1 = new TextRange(text1.Document.ContentStart, text1.Document.ContentEnd).Text;
+            var textR2 = new TextRange(text2.Document.ContentStart, text2.Document.ContentEnd).Text;
+
+            if (caseSens.IsChecked.Value)
+            {
+                textR1 = textR1.ToLower();
+                textR2 = textR2.ToLower();
+            }
+            if (trimSens.IsChecked.Value)
+            {
+                textR1 = textR1.Trim();
+                textR2 = textR2.Trim();
+            }
+
+            var result = Levenshtein.LevenshteinMatch(textR1, textR2);
+            resultTxb.Content = Math.Round(result * 100, 2) + "%";
+        }
+    }
+}
